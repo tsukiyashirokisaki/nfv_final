9b7fc2e733ec839172d78a52d939a4cf7436feeb
tsukiyashirokisaki <55571376+tsukiyashirokisaki@users.noreply.github.com>
2022-01-17 14:31:13 +0800

    add client

--- client.py
+++ client.py
@@ -18,22 +18,20 @@ if __name__ == '__main__':
     n = len(input_files)
     rs = grequests.map([grequests.post('http://127.0.0.2:%d/map'%(ports[i%num_ports]),
     json={"value":input_files[slice(i,n,num_ports)]}) for i in range(num_ports)])
-    map_responses = [rs[i].json()["value"][0] for i in range(num_ports)]
-    partitioned_data = partition(itertools.chain(*map_responses))
-    # n = len(partitioned_data)
-    # rs = []
-    # for i in range(num_ports):
-    #     rs.append(grequests.post('http://127.0.0.2:%d/reduce'%(ports[i%num_ports]),
-    #     json={"value":partitioned_data[slice(i,n,num_ports)]}))
-    # reduce_response = grequests.map(rs)
-    # word_counts = []
-    # for i in range(num_ports):
-    #     word_counts += reduce_response[i].json()["value"]
-    word_counts = collections.defaultdict(int)
-    
-    for key, value in itertools.chain(*map_responses):
-        word_counts[key] += 1
-    word_counts = list(word_counts.items())
+    map_responses = []
+    for i in range(num_ports):
+        for j in range(len(rs[i].json()["value"])):
+            map_responses += rs[i].json()["value"][j]
+    partitioned_data = partition(map_responses)
+    n = len(partitioned_data)
+    rs = []
+    for i in range(num_ports):
+        rs.append(grequests.post('http://127.0.0.2:%d/reduce'%(ports[i%num_ports]),
+        json={"value":partitioned_data[slice(i,n,num_ports)]}))
+    reduce_response = grequests.map(rs)
+    word_counts = []
+    for i in range(num_ports):
+        word_counts += reduce_response[i].json()["value"]
     word_counts.sort(key=operator.itemgetter(1))
     word_counts.reverse()
     top20 = word_counts[:20]
