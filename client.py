74d2b3369a41d37353f4bdef754bb237049798d3
tsukiyashirokisaki <55571376+tsukiyashirokisaki@users.noreply.github.com>
2022-01-17 13:18:53 +0800

    add client

--- client.py
+++ client.py
@@ -23,14 +23,14 @@ if __name__ == '__main__':
     ports = [5000,8000]
     num_ports = len(ports)
     n = len(input_files)
-    rs = grequests.map([grequests.post('http://127.0.0.1:%d/map'%(ports[i%num_ports]),
+    rs = grequests.map([grequests.post('http://127.0.0.2:%d/map'%(ports[i%num_ports]),
     json={"value":input_files[slice(i,n,num_ports)]}) for i in range(num_ports)])
     map_responses = [rs[i].json()["value"][0] for i in range(num_ports)]
     partitioned_data = partition(itertools.chain(*map_responses))
     n = len(partitioned_data)
     rs = []
     for i in range(num_ports):
-        rs.append(grequests.post('http://127.0.0.1:%d/reduce'%(ports[i%num_ports]),
+        rs.append(grequests.post('http://127.0.0.2:%d/reduce'%(ports[i%num_ports]),
         json={"value":partitioned_data[slice(i,n,num_ports)]}))
     reduce_response = grequests.map(rs)
     word_counts = []
